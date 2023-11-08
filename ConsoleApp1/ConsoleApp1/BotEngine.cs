31be14be7ea1aa292f6f14814d035f6b7c31589b
hiccup <hiccup1803@gmail.com>
2023-11-08 23:18:03 +0200

    TelegramBot

---
+++ ConsoleApp1/ConsoleApp1/BotEngine.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace ConsoleApp1
+{
+    internal class BotEngine
+    {
+        private readonly TelegramBotClient _botClient;
+        public BotEngine(TelegramBotClient botClient)
+        {
+            _botClient = botClient;
+        }
+        public async Task ListenForMessagesAsync()
+        {
+            var cts = new CancellationTokenSource();
+
+            var receiverOptions = new ReceiverOptions
+            {
+                AllowedUpdates = Array.Empty<UpdateType>() // receive all update types
+            };
+            _botClient.StartReceiving(
+                updateHandler: HandleUpdateAsync,
+                pollingErrorHandler: HandlePollingErrorAsync,
+                receiverOptions: receiverOptions,
+                cancellationToken: cts.Token
+            );
+
+            var me = await _botClient.GetMeAsync();
+
+            Console.WriteLine($"Start listening for @{me.Username}");
+            Console.ReadLine();
+        }
+
+        private async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            // Only process Message updates
+            if (!(update.Message is var message))
+            {
+                return;
+            }
+
+            // Only process text messages
+            if (!(message.Text is var messageText))
+            {
+                return;
+            }
+
+            Console.WriteLine($"Received a '{messageText}' message in chat {message.Chat.Id}.");
+            var userId = message.Chat.Id;
+            Console.WriteLine($"User ID: {userId}");
+            SendMessageAsync(messageText, userId);
+        }
+
+        private Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            var ErrorMessage = exception switch
+            {
+                ApiRequestException apiRequestException
+                    => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+                _ => exception.ToString()
+            };
+
+            Console.WriteLine(ErrorMessage);
+            return Task.CompletedTask;
+        }
+
+        public void SendMessageAsync(string messageText, long userId)
+        {
+            _botClient.SendTextMessageAsync(userId, messageText);
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,9 @@
+﻿using ConsoleApp1;
+using Telegram.Bot;
+var botClient = new TelegramBotClient("6461579255:AAFq_NIMcyQ2d5Kp0zwUD1r6DmqaeAp6kGY");
+
+// Create a new bot instance
+var metBot = new BotEngine(botClient);
+
+// Listen for messages sent to the bot
+await metBot.ListenForMessagesAsync();
